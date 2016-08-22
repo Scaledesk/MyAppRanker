28ba1ba98f8f425691ae84f5593769cc4062efae
nkscoder <nkscoder@gmail.com>
2016-08-22 15:39:15 +0530

    first commit

---
+++ scripts/contact.php
@@ -0,0 +1,67 @@
+<?php
+  
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+ 
+  $contactname=$_POST['name'];
+
+  $contactemail=$_POST['email'];
+  $contactmessage=$_POST['message'];
+
+
+  
+
+  $emailadmin="sells@myappranker.com";
+  $forwordEmail="myappranker@gmail.com";
+
+  $subject = "Contact Us.";
+  $message ='<html>
+<body>
+<div id="abcd" style="text-align:justify;font-size:18px;"> Name:-'.$contactname.'<br> Email:-'.$contactemail.'<br>Message:-'.$contactmessage.'</div>
+
+
+</body>
+</html>';
+
+
+
+
+
+  /*$messageUsers=file_get_contents('template.html');*/
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers = "From:sudo@scaledesk.com\r\n";
+  $headers = "Content-type: text/html;charset=iso-8859-1" . "\r\n";
+
+  if(mail($emailadmin,$subject,$message,$headers))
+
+  {
+
+    if(mail($forwordEmail,$subject,$message,$headers)){
+
+     
+               header('Location:'.'http://myappranker.com/thankyou.html');
+
+      
+
+    }
+
+    else{
+    
+
+        header('Location:'.'http://myappranker.com/thankyou.html');
+
+    }
+
+
+
+  }
+
+  else{
+   
+     header('Location:'.'http://myappranker.com/');
+
+  }
+
+
+}
+?>
